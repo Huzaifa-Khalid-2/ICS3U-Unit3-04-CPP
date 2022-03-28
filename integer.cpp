81ebbc4ecb4a82db22db49ec277ae83206725727
Huzaifa-Khalid <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-03-28 22:32:39 +0000

    edited code

---
+++ integer.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2022 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Huzaifa Khalid
+// Created on: March 2022
+// This program tells you if the number is 
+// positive/negative/0 with user input
+
+#include <iostream>
+
+int main() {
+    // this function checks if the number 
+    // is >0/ <0/==0
+    int number;
+
+    // input
+    std::cout << "Enter a integer: ";
+    std::cin >> number;
+
+    // process and output
+    if (number > 0) {
+        std::cout << "You have a positive number";
+    } else if (number < 0) {
+        std::cout << "You have a negative number";
+    } else if (number == 0) {
+        std::cout << "Your number is 0";
+    } else {
+      std::cout << "I have no clue";
+  } std::cout << "\nDone.";
+}
